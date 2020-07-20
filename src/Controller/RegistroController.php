b08e1ec3a8f51a727f74c25b48cdec6483dda438
Crisme24 <crisme-24@hotmail.com>
2020-07-20 15:14:07 +0200

    add first user register in the BBDD

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -2,19 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistroController extends AbstractController
 {
     /**
      * @Route("/registro", name="registro")
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $user->setBaneado(false);
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($passwordEncoder->encodePassword($user, $form['password']->getData()));
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('exito', 'Se ha registrado correctamente');
+            return $this->redirectToRoute('registro');
+        }
+
         return $this->render('registro/index.html.twig', [
             'controller_name' => 'My First Controller with Symfony 5',
-            'MiVariable'=> 'Hello World',
+            'formulario'=> $form->createView(),
         ]);
     }
 }
